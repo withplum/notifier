204877d1c4b8b1686e6b8f36dce4070ceddb2abb
Chris Margonis <chris.margonis@withplum.com>
2021-10-15 13:22:58 +0300

    Add parameters in consumer usage

--- consumer/build.gradle.kts
+++ consumer/build.gradle.kts
@@ -6,4 +6,8 @@ plugins {
 configure<com.withplum.notifier.extensions.NotifierExtension> {
     reportingEnabled.value(true)
     versionsFilePath.set(rootDir.path + File.separator + "versions.properties")
+    githubInstallationId.set("numeric_id")
+    githubRepository.set("<repository>")
+    githubRepositorySlug.set("/repos/<org-name>/<repository>")
+    githubIssueId.set("<issue-id>")
 }
