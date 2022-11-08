0dce645e4d78a302641796e7c716cf65edb2575a
Chris Margonis <chris.margonis@withplum.com>
2022-11-08 16:04:57 +0200

    Update refreshVersions to version 0.51.0

--- notifier-plugin/settings.gradle.kts
+++ notifier-plugin/settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "notifier-plugin"
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.23.0"
+    id("de.fayard.refreshVersions") version "0.51.0"
 }
